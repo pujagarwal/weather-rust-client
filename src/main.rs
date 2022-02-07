d78db15993059cd5a429f517001b673cc940e848
Puja Agarwal <agarwalpuja018@gmail.com>
2022-02-06 23:40:00 -0800

    rust update

--- src/main.rs
+++ src/main.rs
@@ -12,25 +12,58 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         "#;
 
     // Deserialize the hardcoded JSON into a Weather struct
+    
     let weather1: model::Weather = serde_json::from_str(json).unwrap();
 
     println!("\nWeather from a JSON we hard-coded locally:\n{:?}", weather1);
-
+    
     //
     // Now that we know we can deserialize a hard-coded JSON into a struct model,
     // let's see if we can fetch the weather from the backend.
     //
-
+    
     let client = reqwest::Client::new();
+    let p = model::User {
+        username: "Puja".into(),
+        password: "123".into(),
+      };
+    
+    let res = reqwest::Client::new()
+    .post("http://54.234.54.104:3000/v1/auth")
+    .json(&p)
+    .send()
+    .await?;
 
+    let js = res.json::<model::UserResponse>().await?;
+    
+    
+    // println!("Access token: {:#?}", js.access_token);
     let response = client
-        .get("https://api.openweathermap.org/data/2.5/weather?q=corvallis&appid=b98e3f089c86867862f28236d174368a&&units=imperial")
-        .send()
-        .await?;
+    .get("http://54.234.54.104:3000/v1/weather")
+    .header("Authorization", "Bearer ".to_owned() + &js.access_token)
+    .send()
+    .await?;
 
-    let weather2 = response
-        .json::<model::Weather>()
-        .await?;
+    let weather2 = response.json::<model::Weather>().await?;
+
+     println!(
+        "\nWeather from server running on ec2 instance:\n {:?}",
+        weather2
+    );
+
+    // fetch greeting
+    let response = client
+    .get("http://54.234.54.104:3000/v1/hello")
+    .header("Authorization", "Bearer ".to_owned() + &js.access_token)
+    .send()
+    .await?;
+
+    let greeting = response.json::<model::GreetingResponse>().await?;
+
+     println!(
+        "\nGreeting from server running on ec2 instance:\n {:?}",
+        greeting.greeting
+     );
 
     println!("\nWeather from openweathermap.org:\n {:?}", weather2);
 
--- src/model.rs
+++ src/model.rs
@@ -4,7 +4,8 @@
 //   In action: https://app.quicktype.io
 //
 
-use serde::{Serialize, Deserialize};
+//use serde::{Serialize, Deserialize};
+use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Weather {
@@ -14,4 +15,21 @@ pub struct Weather {
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Main {
     temp: f64,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct User {
+    pub username: String,
+    pub password: String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct UserResponse {
+    pub access_token: String,
+    pub expires: String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct GreetingResponse {
+    pub greeting: String,
 }
